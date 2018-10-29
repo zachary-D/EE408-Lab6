e2b30af31d56a3fb006b0b8d58c67d4562de176f
Unknown <zacharyd1@live.com>
2018-10-28 23:16:44 -0400

    Implemented subtraction/multiplication/division

--- Calc/Calc/app/src/main/java/edu/clarkson/ee408/MainActivity.java
+++ Calc/Calc/app/src/main/java/edu/clarkson/ee408/MainActivity.java
@@ -31,81 +31,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b0 = (Button)findViewById(R.id.button0);
-        b1 = (Button)findViewById(R.id.button1);
-        b2 = (Button)findViewById(R.id.button2);
-        b3 = (Button)findViewById(R.id.button3);
-        b4 = (Button)findViewById(R.id.button4);
-        b5 = (Button)findViewById(R.id.button5);
-        b6 = (Button)findViewById(R.id.button6);
-        b7 = (Button)findViewById(R.id.button7);
-        b8 = (Button)findViewById(R.id.button8);
-        b9 = (Button)findViewById(R.id.button9);
-        bPlus = (Button)findViewById(R.id.buttonPlus);
-        bMinus = (Button)findViewById(R.id.buttonMinus);
-        bMult = (Button)findViewById(R.id.buttonMult);
-        bDiv = (Button)findViewById(R.id.buttonDiv);
-        bClear = (Button)findViewById(R.id.buttonClear);
-        bEq = (Button)findViewById(R.id.buttonEq);
+        b0 = (Button) findViewById(R.id.button0);
+        b1 = (Button) findViewById(R.id.button1);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
+        b5 = (Button) findViewById(R.id.button5);
+        b6 = (Button) findViewById(R.id.button6);
+        b7 = (Button) findViewById(R.id.button7);
+        b8 = (Button) findViewById(R.id.button8);
+        b9 = (Button) findViewById(R.id.button9);
+        bPlus = (Button) findViewById(R.id.buttonPlus);
+        bMinus = (Button) findViewById(R.id.buttonMinus);
+        bMult = (Button) findViewById(R.id.buttonMult);
+        bDiv = (Button) findViewById(R.id.buttonDiv);
+        bClear = (Button) findViewById(R.id.buttonClear);
+        bEq = (Button) findViewById(R.id.buttonEq);
 
         display = (TextView) findViewById(R.id.display);
 
         View.OnClickListener lNum = new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Button b = (Button)v;
-                    if (calc){
-                        calc=false;
-                        display.setText("");
-                    }
-                    if (op!=-1 && !op1Set){
-                        operand1 = Integer.parseInt(display.getText().toString());
-                        op1Set=true;
-                        display.setText("");
-                    }
-
-                    String text;
-                    text = display.getText().toString()+b.getText().toString();
-                    display.setText(text);
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (calc) {
+                    calc = false;
+                    display.setText("");
                 }
-            };
+                if (op != -1 && !op1Set) {
+                    operand1 = Integer.parseInt(display.getText().toString());
+                    op1Set = true;
+                    display.setText("");
+                }
+
+                String text;
+                text = display.getText().toString() + b.getText().toString();
+                display.setText(text);
+            }
+        };
+
         View.OnClickListener lOp = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button b = (Button)v;
+                Button b = (Button) v;
 
-                if (op!=-1 && op1Set){
+                if (op != -1 && op1Set) {
                     int result = 0;
-                    switch (op){
+                    switch (op) {
                         case 0: // +
-                            result = operand1+Integer.parseInt(display.getText().toString());
+                            result = operand1 + Integer.parseInt(display.getText().toString());
                             break;
                         case 1: // -
-                            result = operand1-Integer.parseInt(display.getText().toString());
+                            result = operand1 - Integer.parseInt(display.getText().toString());
                             break;
                         case 2: // *
-                            result = operand1*Integer.parseInt(display.getText().toString());
+                            result = operand1 * Integer.parseInt(display.getText().toString());
                             break;
                         case 3: // /
-                            result = operand1/Integer.parseInt(display.getText().toString());
+                            result = operand1 / Integer.parseInt(display.getText().toString());
                             break;
                     }
-                    op=-1;
-                    op1Set=false;
+                    op = -1;
+                    op1Set = false;
                     display.setText(Integer.toString(result));
                 }
-                if (op==-1&&!calc){
-                    if (b.getText().equals("+")){
-                        op=0;
+                if (op == -1 && !calc) {
+                    if (b.getText().equals("+")) {
+                        op = 0;
                     }
-                    if (b.getText().equals("-")){
-                        op=1;
+                    if (b.getText().equals("-")) {
+                        op = 1;
                     }
-                    if (b.getText().equals("*")){
-                        op=2;
+                    if (b.getText().equals("*")) {
+                        op = 2;
                     }
-                    if (b.getText().equals("/")){
-                        op=3;
+                    if (b.getText().equals("/")) {
+                        op = 3;
                     }
                 }
             }
@@ -115,8 +116,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (op1Set) {
-                    // note: need to expand to handle -,*, and /!!!
-                    int result = operand1 + Integer.parseInt(display.getText().toString());
+                    int result;
+                    switch (op) {
+                        case (0):
+                            // note: need to expand to handle -,*, and /!!!
+                            result = operand1 + Integer.parseInt(display.getText().toString());
+                            break;
+
+                        case (1):
+                            result = operand1 - Integer.parseInt(display.getText().toString());
+                            break;
+
+                        case (2):
+                            result = operand1 * Integer.parseInt(display.getText().toString());
+                            break;
+
+                        case (3):
+                            result = operand1 / Integer.parseInt(display.getText().toString());
+                            break;
+
+                        default:
+                            result = 0; //In case things go bad.
+                    }
                     display.setText(Integer.toString(result));
                     op = -1;
                     op1Set = false;
